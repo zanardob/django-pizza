bcd68836f47b7868abe1a8fe5d020f3b427f72e8
zanardob <guilherme.borduchi@usp.br>
2015-08-18 16:51:38 -0300

    Menu added

--- pizzeria/pizzeria/urls.py
+++ pizzeria/pizzeria/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+
+from django.conf import settings
+from django.conf.urls import include, url, static
 from django.contrib import admin
 
+from pizza.views import menu
+
 urlpatterns = [
+    url(r'menu$', menu),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
