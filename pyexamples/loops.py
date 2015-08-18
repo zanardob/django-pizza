321b0a65c0071dbbb0dbff677a8a545938e8c3c7
zanardob <guilherme.borduchi@usp.br>
2015-08-18 10:50:06 -0300

    Added loops

---
+++ pyexamples/loops.py
@@ -0,0 +1,13 @@
+orders = [
+    {
+        'name': 'Mario',
+        'flavor': 'pepperoni'
+    },
+    {
+        'name': 'Marco',
+        'flavor': 'ham'
+    }
+]
+
+for order in orders:
+    print(order['name'], order['flavor'])
