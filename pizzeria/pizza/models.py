6dd3401af2e306b534ba6ef7e247f667ce27b4cc
zanardob <guilherme.borduchi@usp.br>
2015-08-18 15:29:31 -0300

    Started Django

---
+++ pizzeria/pizza/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Pizza(models.Model):
+    flavor = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    ingredients = models.TextField()
+
+    def __unicode__(self):
+        return u'{} Pizza'.format(self.flavor)
+
+    def __repr__(self):
+        return unicode(self)
