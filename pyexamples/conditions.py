556eb112091282cacfff327a1ff0237ab9d28a5d
zanardob <guilherme.borduchi@usp.br>
2015-08-18 09:41:02 -0300

    Conditionals

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -1,2 +1,17 @@
 
 print('Hello World')
+
+sauce = 'tomato'
+
+order_description = """
+This is a special order.
+The client only wants the pasta.
+"""
+
+if sauce.startswith('toma'):
+    print('it is probably tomato...')
+else:
+    print('not tomato')
+
+if order_description:
+    print(order_description)
