0ef925a614c266a3b357ce3e820916ee57f97772
zanardob <guilherme.borduchi@usp.br>
2015-08-18 18:07:10 -0300

    Fix in views

--- pizzeria/pizza/views.py
+++ pizzeria/pizza/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from .models import Pizza
 
 def menu(request):
-    import ipdb; ipdb.set_trace()
     pizzas = Pizza.objects.all()
     context = {'pizzas': pizzas}
     return render(request, 'pizza/menu.html', context)
